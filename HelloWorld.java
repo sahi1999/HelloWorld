98f8aea1cedad3e77df764d90c4251d61ea9e374
sahi1999 <sahi63715@gmail.com>
2021-08-21 17:42:06 +0530

    Added A HelloWorld Program

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class HelloWorld
+{
+    public static void main(String args[])
+    {
+        int n1= 4;
+	int n2 = 9;
+	System.out.println("Addition =" + (n1 +n2));
+	System.out.println("Division = " +(n1/n2));
+	System.out.println("Multiplication = " +(n1*n2));
+	System.out.println("Subtraction = "+(n1-n2));
+
+    }
+}
